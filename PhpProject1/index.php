6201f8b8127c2cdc888ba8542e4ea9ed3229d004
matthias <matthias@mtux>
2017-02-05 22:00:29 +0100

    justtesting

--- PhpProject1/index.php
+++ PhpProject1/index.php
@@ -12,7 +12,7 @@ and open the template in the editor.
     <body>
         <?php
         // put your code here
-        echo 'well just testing!';
+        echo 'well just testing! - 2159';
         ?>
     </body>
 </html>
